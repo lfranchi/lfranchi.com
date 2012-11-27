b52ac4feb5c785a010db85448197d46adbf1d147
Leo Franchi <lfranchi@kde.org>
2012-11-26 23:10:27 -0500

    Switch to using compass for css

---
+++ compass-config.rb
@@ -0,0 +1,8 @@
+# This is the Compass configuration file. The configuration file for nanoc is 
+# named “config.yaml”.
+
+project_path = File.dirname(__FILE__)
+http_path    = '/'
+output_style = :expanded
+sass_dir     = 'content/style'
+css_dir      = 'output/style'
